2a0a90e65c560547ba0e61b102061787733734c3
Duckz-TSE <146405997+Duckz-TSE@users.noreply.github.com>
2026-07-04 05:30:31 -0700

    Add: longest-substring-without-duplicates - submission-17

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-17.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = {}
+        left = 0
+        longest = 0
+
+        for right, char in enumerate(s):
+            if char in seen and seen[char] >= left:
+                left = seen[char] + 1
+            seen[char] = right
+            longest = max(longest, (right - left + 1))
+
+        return longest
